93b82c3a6542e122c747e710d6cb159d5bb4caf5
rBaun <runeb95@gmail.com>
2024-01-21 00:03:20 +0100

    Removed unused exception and implemented exception handlers for customer

--- src/main/java/com/rbaun/banking/exception/customer/CustomerAlreadyExist.java
+++
@@ -1,9 +0,0 @@
-package com.rbaun.banking.exception.customer;
-
-public class CustomerAlreadyExist extends RuntimeException {
-
-    public CustomerAlreadyExist(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/com/rbaun/banking/exception/customer/CustomerExceptionHandler.java
+++ src/main/java/com/rbaun/banking/exception/customer/CustomerExceptionHandler.java
@@ -1,4 +1,48 @@
 package com.rbaun.banking.exception.customer;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class CustomerExceptionHandler {
+
+    @ExceptionHandler(CustomerInvalidEmailException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleCustomerInvalidEmailException(CustomerInvalidEmailException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(CustomerInvalidPhoneNumberException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleCustomerInvalidPhoneNumberException(CustomerInvalidPhoneNumberException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleCustomerNotFoundException(CustomerNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateEmailException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<String> handleDuplicateEmailException(DuplicateEmailException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DuplicatePhoneNumberException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<String> handleDuplicatePhoneNumberException(DuplicatePhoneNumberException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateSocialSecurityNumberException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<String> handleDuplicateSocialSecurityNumberException(DuplicateSocialSecurityNumberException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
 }
