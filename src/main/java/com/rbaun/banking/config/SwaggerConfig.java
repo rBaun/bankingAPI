65a71da606281998fb1a310bedc75664af38b139
rBaun <runeb95@gmail.com>
2024-02-13 12:11:23 +0100

    Updated the Swagger configuration such that the swagger ui provides token authorization possibility and the context path has properly been changed such that versioning can be made easier in the future

--- src/main/java/com/rbaun/banking/config/SwaggerConfig.java
+++ src/main/java/com/rbaun/banking/config/SwaggerConfig.java
@@ -1,18 +1,42 @@
 package com.rbaun.banking.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Bean
+    public OpenAPI setupBaseApi() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .addServersItem(new Server().url(contextPath));
+    }
+
     @Bean
     public GroupedOpenApi api() {
         return GroupedOpenApi.builder()
                 .group("Banking API v1")
-                .pathsToMatch("/api/v1/**")
+                .pathsToMatch("/v1/**")
                 .build();
     }
-
 }
